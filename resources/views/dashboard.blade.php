f11f01b330d3d8937409e1cfe212318e3095da26
Nastu94 <vittoriosoligo94@hotmail.it>
2024-10-20 12:13:38 +0200

    aggiornata la vista dashboard per gli utenti amministratore

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,47 +21,50 @@
             <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 text-gray-900 dark:text-gray-100">
                     <div class="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        <!-- Icona Gestisci Prenotazioni -->
-                        <a href="{{ route('prenotazioni.create') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-document-text class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Gestisci Prenotazioni</h3>
-                        </a>
-
-                        <!-- Icona Le Mie Prenotazioni -->
-                        <a href="{{ route('prenotazioni.index-personali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-calendar-days class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Le mie Prenotazioni</h3>
-                        </a>
-
-                        <!-- Icona Prenotazioni Globali -->
-                        <a href="{{ route('prenotazioni.index-globali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-uiw-global class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Prenotazioni Globali</h3>
-                        </a>
-
-                        <!-- Icona Prenotazioni Aziendali -->
-                        <a href="{{ route('prenotazioni.index-aziendali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-building-office class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Prenotazioni Aziendali</h3>
-                        </a>
                         
-                        <!-- Icona Prenotazioni Condivise -->
-                        <a href="{{ route('prenotazioni.index-condivise') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-share class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Prenotazioni Condivise</h3>
-                        </a>
+                        @if(Auth::user()->permessi->contains('nome', 'Azienda') || Auth::user()->permessi->contains('nome', 'Autista'))
+                            <!-- Icona Gestisci Prenotazioni -->
+                            <a href="{{ route('prenotazioni.create') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-document-text class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Gestisci Prenotazioni</h3>
+                            </a>
 
-                        <!-- Icona Registro Prenotazioni -->
-                        <a href="{{ route('registro.index', ['data' => \Carbon\Carbon::now()->format('Y-m-d')]) }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-clipboard-document-list class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Registro Prenotazioni</h3>
-                        </a>
+                            <!-- Icona Le Mie Prenotazioni -->
+                            <a href="{{ route('prenotazioni.index-personali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-calendar-days class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Le mie Prenotazioni</h3>
+                            </a>
 
-                        <!-- Icona Foglio di Viaggio -->
-                        <a href="{{ route('fogli-viaggio.index') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
-                            <x-heroicon-s-newspaper class="h-20 w-20 mb-2"/>
-                            <h3 class="text-lg font-semibold">Fogli di Viaggio</h3>
-                        </a>
+                            <!-- Icona Prenotazioni Globali -->
+                            <a href="{{ route('prenotazioni.index-globali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-uiw-global class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Prenotazioni Globali</h3>
+                            </a>
+
+                            <!-- Icona Prenotazioni Aziendali -->
+                            <a href="{{ route('prenotazioni.index-aziendali') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-building-office class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Prenotazioni Aziendali</h3>
+                            </a>
+                            
+                            <!-- Icona Prenotazioni Condivise -->
+                            <a href="{{ route('prenotazioni.index-condivise') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-share class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Prenotazioni Condivise</h3>
+                            </a>
+
+                            <!-- Icona Registro Prenotazioni -->
+                            <a href="{{ route('registro.index', ['data' => \Carbon\Carbon::now()->format('Y-m-d')]) }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-clipboard-document-list class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Registro Prenotazioni</h3>
+                            </a>
+
+                            <!-- Icona Foglio di Viaggio -->
+                            <a href="{{ route('fogli-viaggio.index') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-newspaper class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Fogli di Viaggio</h3>
+                            </a>                        
+                        @endif
 
                         @if(Auth::user()->permessi->contains('nome', 'Azienda'))
                             <!-- Icona Gestione Autisti -->
@@ -76,6 +79,22 @@
                                 <h3 class="text-lg font-semibold">Gestione Veicoli</h3>
                             </a>
                         @endif
+
+                        @if(Auth::user()->permessi->contains('nome', 'Amministratore'))
+                            <!-- Icona Gestione Aziende -->
+                            <a href="{{ route('admin.create-azienda') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-building-office class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Gestione Aziende</h3>
+                            </a>
+
+                            <!-- Icona Gestione Utenti Azienda -->
+                            <a href="{{ route('admin.create-user') }}" class="flex flex-col items-center p-4 rounded-lg shadow-lg hover:bg-gray-100 dark:hover:bg-gray-700 transition">
+                                <x-heroicon-s-user-group class="h-20 w-20 mb-2"/>
+                                <h3 class="text-lg font-semibold">Gestione Utenti Azienda</h3>
+                            </a>
+                        @endif
+
+                        
                     </div>
                 </div>
             </div>
